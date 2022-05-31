a60803e1fadeb8cc6eda483206bd01d83307d4d4
Samox <sammy.teillet@gmail.com>
2022-05-31 12:08:00 +0200

    feature(): Simplify API

--- bin/lcp.js
+++ bin/lcp.js
@@ -4,13 +4,13 @@ var lcp = require('../lib/index.js');
 var commandLineArgs = require('command-line-args');
 
 var optionDefinitions = [
-  { name: 'port', alias: 'p', type: Number, defaultValue: 8010 },
+  { name: 'proxyPort', alias: 'p', type: Number, defaultValue: 8010 },
   {
     name: 'proxyPartial',
     type: String,
     defaultValue: '/proxy'
   },
-  { name: 'proxyUrl', type: String, defaultValue: 'http://localhost:9000' },
+  { name: 'port', type: String, defaultValue: '9000' },
   { name: 'credentials', type: Boolean, defaultValue: false },
   { name: 'origin', type: String, defaultValue: '*' },
   { name: 'webhookStore', type: String }
@@ -21,7 +21,7 @@ try {
   if (!options.webhookStore) {
     throw new Error('--webhookStore is required. example --webhookStore https://claimyourdomain.webhook.store');
   }
-  lcp.startProxy(options.port, options.proxyUrl, options.proxyPartial, options.credentials, options.origin, options.webhookStore);
+  lcp.startProxy(options.proxyPort, 'http://localhost:'+options.port, options.proxyPartial, options.credentials, options.origin, options.webhookStore);
 } catch (error) {
   console.error(error);
 }
